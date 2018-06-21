ecf2bf9602e25041eb35d9403c935b84f3ccc708
Connor Hull <chull@optimumhris.com>
2018-06-21 16:50:36 -0500

    First go at DeploymentManager

--- DeploymentManager.py
+++ DeploymentManager.py
@@ -1,6 +1,11 @@
+import subprocess, os, json
+from distutils.dir_util import copy_tree
+
 class DeploymentManager:
-    def __init__(self, repoconfig):
-        self.repoconfig = repoconfig
+    def __init__(self):
+        # yeah this is not a very good solution. can't find a good way to structure the dependencies
+        with open("config.json", "r") as f:
+            self.repoconfig = json.loads(f.readlines())
 
     def receivepush(self, pushdata):
         pushedrepo = pushdata["repository"]["full_name"]
@@ -15,4 +20,13 @@ class DeploymentManager:
             print("Received a push for a repo with no configuration! (" + pushedrepo + "). Check that you have a definition for this repo in your config.")
             return
 
+        myenv = os.environ.copy()
+
+        deploykey = foundrepo.get("deploykeypath", "")
+        if deploykey:
+            myenv["GIT_SSH_COMMAND"] = "ssh -i " + deploykey
         
+        subprocess.run("git reset --hard origin/master".split(), cwd=foundrepo["repodirectory"], env=myenv)
+        subprocess.run("git pull origin master".split(), cwd=foundrepo["repodirectory"], env=myenv)
+
+        copy_tree(os.path.expanduser(foundrepo["repodirectory"]), os.path.expanduser(foundrepo["contentdirectory"]))
